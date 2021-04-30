7b3cc1105172c641b39a2ddab564f292974c71eb
Tintu Jose <tintu.jose@yaelev.se>
2021-04-30 11:13:51 +0200

    Implemented Buffer Insert & Extract

--- lib/buffer/buffer.c
+++ lib/buffer/buffer.c
@@ -1,9 +1,82 @@
 #include "buffer.h"
 
+#define NUM_BITS 8
+
+//Function to insert value to the buffer
 void buffer_insert(uint8_t *buffer, uint8_t start, uint8_t length, uint64_t value)
 {
+    uint8_t start_bit, end_bit, data, position;
+    int index = start / NUM_BITS;
+    position = start % NUM_BITS;
+    uint64_t allones = ~0;
+
+    while (length)
+    {
+        if ((NUM_BITS - position) < length)
+        {
+            end_bit = NUM_BITS - 1;
+            start_bit = position;
+            data = value & (allones >> (NUM_BITS - position));
+            value >>= (NUM_BITS - position);
+            length -= (NUM_BITS - position);
+            position = 0;
+        }
+        else
+        {
+            start_bit = position;
+            end_bit = position + length - 1;
+            data = value;
+            length = 0;
+        }
+
+        // Prepare the mask
+        uint8_t left = allones << (end_bit + 1);
+        uint8_t right = ((1 << start_bit) - 1);
+        uint8_t mask = left | right;
+
+        // Clear bits start_bit through end_bit
+        uint8_t byte_cleared = buffer[index] & mask;
+
+        // Move data into correct position.
+        uint8_t data_shifted = data << start_bit;
+
+        // Update buffer with new value
+        buffer[index] = (byte_cleared | data_shifted);
+        index++;
+    }
 }
 
+//Function to extract value from the buffer
 uint64_t buffer_extract(uint8_t *buffer, uint8_t start, uint8_t length)
 {
+    int extract_len = 0;
+    uint64_t value = 0;
+    int index = start / NUM_BITS;
+    uint8_t position = start % NUM_BITS;
+    uint8_t tmp, shift = 0;
+    uint8_t remlen = length;
+
+    while (remlen)
+    {
+        if ((NUM_BITS - position) < remlen)
+        {
+            extract_len = NUM_BITS - position;
+            tmp = (((1 << extract_len) - 1) & (buffer[index] >> position));
+            shift = length - remlen;
+            remlen -= extract_len;
+            position = 0;
+        }
+        else
+        {
+            extract_len = remlen;
+
+            shift = length - remlen;
+            tmp = (((1 << extract_len) - 1) & (buffer[index] >> position));
+            remlen = 0;
+        }
+        value |= (uint64_t)tmp << shift;
+        index++;
+    }
+
+    return value;
 }
--- test/buffer_test.c
+++ test/buffer_test.c
@@ -1,13 +1,57 @@
 #include "unity.h"
 #include "buffer.h"
+#include <stdio.h>
+#define BUFF_SIZE 30
+
+uint8_t buf[BUFF_SIZE] = {
+    0x0,
+};
 
 void setUp(void) {}
 
 void tearDown(void) {}
 
+//Testing insertion and extraction from the same index
+void test_buffer_index(void)
+{
+    buffer_insert(buf, 8, 5, 31);
+    TEST_ASSERT_EQUAL_UINT64(31, buffer_extract(buf, 8, 5));
+    buffer_insert(buf, 24, 8, 176);
+    TEST_ASSERT_EQUAL_UINT64(176, buffer_extract(buf, 24, 8));
+    buffer_insert(buf, 4, 1, 1);
+    TEST_ASSERT_EQUAL_UINT64(1, buffer_extract(buf, 4, 1));
+}
+
+//Testing buffer insert and extract
+void test_buffer_functions(void)
+{
+    buffer_insert(buf, 12, 6, 24);
+    buffer_insert(buf, 20, 13, 0x1234);
+    TEST_ASSERT_EQUAL_UINT64(0x1234, buffer_extract(buf, 20, 13));
+    TEST_ASSERT_EQUAL_UINT64(24, buffer_extract(buf, 12, 6));
+
+    buffer_insert(buf, 42, 11, 0x2f4);
+    TEST_ASSERT_EQUAL_UINT64(0x2f4, buffer_extract(buf, 42, 11));
+
+    buffer_insert(buf, 10, 8, 235);
+    TEST_ASSERT_EQUAL_UINT64(235, buffer_extract(buf, 10, 8));
+}
+
+// Testing 64 bits
+void test_buffer_64bits(void)
+{
+    buffer_insert(buf, 0, 64, 0x94f6767654789152);
+    TEST_ASSERT_EQUAL_UINT64(0x94f6767654789152, buffer_extract(buf, 0, 64));
+
+    buffer_insert(buf, 80, 64, 0x8756767654789151);
+    TEST_ASSERT_EQUAL_UINT64(0x8756767654789151, buffer_extract(buf, 80, 64));
+}
+
 int main(void)
 {
-    UNITY_BEGIN();
+    RUN_TEST(test_buffer_functions);
+    RUN_TEST(test_buffer_64bits);
+    RUN_TEST(test_buffer_index);
 
     return UNITY_END();
 }
